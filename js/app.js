277182b01df67a3fd6b8902d7f7e8c227b806dc3
hbarrons <hbarrons@gmail.com>
2022-05-22 14:22:44 -0400

    stub up html and start on functionality

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let woodCount = document.getElementById("wood-count")
+let brickCount = document.getElementById("brick-count")
+let wheatCount = document.getElementById("wheat-count")
+let rockCount = document.getElementById("rock-count")
+let sheepCount = document.getElementById("sheep-count")
+
+
+init()
+function init () {
+  woodCount.innerHTML = "0"
+  brickCount.innerHTML = "0"
+  wheatCount.innerHTML = "0"
+  rockCount.innerHTML = "0"
+  sheepCount.innerHTML = "0"
+}
